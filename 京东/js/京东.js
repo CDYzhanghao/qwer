756f93934783ccd1d47c0ff0e07920b5d9ad486f
CDYzhanghao <1517786803@qq.com>
2017-07-07 17:58:56 +0800

    京东

---
+++ 京东/js/京东.js
@@ -0,0 +1,14 @@
+
+// var Swiper = new Swiper('.swiper-container',{
+// loop:true,
+// })
+// $('#btn1').click(function(){
+// alert(mySwiper.realIndex); 
+// })
+
+ var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop:true,
+    });
